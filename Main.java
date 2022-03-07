2cba2af3c7a54281bb5fd2942ece068eff2a7035
Krisna Calixto <krisna.krisnacalixto@gmail.com>
2022-03-07 15:38:24 -0300

    Cadastro de cliente funcionando

--- Main.java
+++ Main.java
@@ -5,51 +5,85 @@ import java.util.ArrayList;
 
 public class Main {
 	static ArrayList<Conta> contas = new ArrayList<Conta>();
+	static ArrayList<Cliente> clientes = new ArrayList<Cliente>();
 
 	public static void main(String[] args) {
-		while(true) {
-			Menu mainMenu =  new Menu("Menu Principal", Arrays.asList("Conta", "Cliente", "Operacoes"));
+		while (true) {
+			Menu mainMenu = new Menu("Menu Principal", Arrays.asList("Conta", "Cliente", "Operacoes"));
 			int op = mainMenu.getSelection();
 
-			if(op == 1) {
+			if (op == 1) {
 				Menu menuConta = new Menu("Menu conta", Arrays.asList("Criar conta", "Listar contas", "Sair"));
-				int op2 = menuConta.getSelection();
+				int opConta = menuConta.getSelection();
 
-				if(op2 == 1) {
+				if (opConta == 1) {
 					CriarConta();
-				}
-				else if(op2 == 2) {
+				} else if (opConta == 2) {
 					ListarContas();
-				}
-				else if(op2 == 3) {
+				} else if (opConta == 3) {
 					System.out.println("Saindo");
 				}
 			}
 
-			if(op == 3) {
+			if (op == 2) {
+				Menu menuCliente = new Menu("Menu cliente", Arrays.asList("Criar cliente", "Listar clientes", "Sair"));
+				int opCliente = menuCliente.getSelection();
+
+				if (opCliente == 1) {
+					CriarCliente();
+				} else if (opCliente == 2) {
+					ListarClientes();
+				} else if (opCliente == 3) {
+					System.out.println("Saindo");
+				}
+			}
+
+			if (op == 3) {
 				Menu menuOperacoes = new Menu("Menu operacoes", Arrays.asList("Saque", "Deposito", "Sair"));
 				int operacao = menuOperacoes.getSelection();
-				if(operacao == 1) {
+				if (operacao == 1) {
 					Saque();
 				}
-				if(operacao == 2) {
+				if (operacao == 2) {
 					Deposito();
 				}
-				if(operacao == 3){
+				if (operacao == 3) {
 					System.out.println("Saindo");
 				}
 			}
 		}
 	}
 
+	public static void CriarCliente() {
+		System.out.println("Criar cliente");
+
+		System.out.print("Nome: ");
+		Scanner s = new Scanner(System.in);
+		String nome = s.nextLine();
+
+		System.out.print("CPF: ");
+		String cpf = s.nextLine();
+
+		System.out.print("Telefone: ");
+		String tel = s.nextLine();
+
+		Cliente newCliente = new Cliente(cpf, nome, tel);
+
+		clientes.add(newCliente);
+	}
+
+	public static void ListarClientes(){
+		
+	}
+
 	public static void CriarConta() {
-    System.out.println("Criar conta");
+		System.out.println("Criar conta");
 
-    System.out.print("Nome: ");
-    Scanner s = new Scanner(System.in);
-    String nome = s.nextLine();
+		System.out.print("Nome: ");
+		Scanner s = new Scanner(System.in);
+		String nome = s.nextLine();
 
-    System.out.print("nrConta: ");
+		System.out.print("nrConta: ");
 		String nrConta = s.nextLine();
 
 		System.out.print("Saldo: ");
@@ -62,11 +96,10 @@ public class Main {
 
 	public static void ListarContas() {
 		System.out.println("Listar contas");
-		if(contas.size() == 0) {
+		if (contas.size() == 0) {
 			System.out.println("Nao existem contas");
-		}
-		else {
-			for(Conta suaConta : contas) {
+		} else {
+			for (Conta suaConta : contas) {
 				suaConta.ListarConta();
 				System.out.println("");
 			}
@@ -80,7 +113,7 @@ public class Main {
 		Scanner s = new Scanner(System.in);
 		String numeroConta = s.nextLine();
 		Conta contaEncontrada = findConta(Integer.parseInt(numeroConta));
-		if(contaEncontrada == null){
+		if (contaEncontrada == null) {
 			System.out.println("Conta nao encontrada");
 			return;
 		}
@@ -97,7 +130,7 @@ public class Main {
 		Scanner s = new Scanner(System.in);
 		String numeroConta = s.nextLine();
 		Conta contaEncontrada = findConta(Integer.parseInt(numeroConta));
-		if(contaEncontrada == null){
+		if (contaEncontrada == null) {
 			System.out.println("Conta nao encontrada");
 			return;
 		}
@@ -114,6 +147,6 @@ public class Main {
 				return conta;
 			}
 		}
-    	return null;
+		return null;
 	}
 }
